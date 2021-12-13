5d299672808426aee6f8b9a3f12561529899e715
Lejcyn <kowalczyk.tomasz00@gmail.com>
2021-12-13 14:35:44 +0100

    New Features
    Added automatic file search and better printing

--- Compare.py
+++ Compare.py
@@ -1,36 +1,45 @@
 import pandas as pd
+from os import listdir
+from os.path import isfile, join
 
-files=['Barcelona.xlsx','London.xlsx','Insbruck.xlsx','Graz.xlsx','Vienna.xlsx','Milano.xlsx','Munich.xlsx','Berlin.xlsx','Zurich.xlsx','Newbury.xlsx','Manchester.xlsx']
+mypath="Cities"
 
+files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+
+
+
+#files=['Barcelona.xlsx','London.xlsx','Insbruck.xlsx','Graz.xlsx','Vienna.xlsx','Milano.xlsx','Munich.xlsx','Berlin.xlsx','Zurich.xlsx','Newbury.xlsx','Manchester.xlsx']
 def CompareLivingCosts(file):
-
-    df = pd.read_excel(file)
-
+    #Read and Parse File
+    df = pd.read_excel("Cities/"+file)
     nan_value = float("NaN")
-
     df.replace("", nan_value, inplace=True)
-
     df.dropna(subset = ["Difference"], inplace=True)
     df.dropna(subset = ["Salary"], inplace=True)
     Differences=df["Difference"].astype(str).values.tolist()
     Salary=df["Salary"].astype(str).values.tolist()
-    kek=[]
+    City=file.replace(".xlsx","")
+    #Parse single records
+    Salaries=Parse(Salary)
+    Difs=Parse(Differences)
+    #Calculate needed Values
+    AverageDifs=sum(Difs)/len(Difs)
+    Salary=Salaries[0]
+    #Printout the results
+    
+    print(City+"\t") 
+    print( "Living costs: "+str(AverageDifs) + "% Salary: " +str(int(Salary))+"%")
+    TotalDif=float(Salary)-AverageDifs
+    print("Total Living quality increase: "+ str(int(TotalDif))+"%\n\n")
 
-    sal=Salary[0]
-    sal1=sal.replace(" ","")
-    sal2=sal1.replace("%","")
-
-    for idx,i in enumerate(Differences):
-        
+#Supportive repeating code
+def Parse(Records):
+    list=[]
+    for i in Records:
         a=i.replace(" ","")
         b=a.replace("%","")
-        kek.append(float(b.replace("\xa0","")))
-    print(file+"\t") 
-    AverageCosts=sum(kek)/len(kek)
-    print( "Living costs: "+str(AverageCosts) + " Salary: " +sal2)
-    Dif=float(sal2)-AverageCosts
-    print("Total Living quality increase: "+ str(Dif))
-    print("Cica will earn: "+ str(300000+300000*Dif/100))
-    print("Kotek will earn: "+ str(600000+600000*Dif/100) +"\n\n")
+        list.append(float(b.replace("\xa0","")))
+    return list
+
 for file in files:
     CompareLivingCosts(file)
